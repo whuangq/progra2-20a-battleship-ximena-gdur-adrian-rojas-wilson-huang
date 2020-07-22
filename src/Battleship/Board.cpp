dc608183634c7640714f59ff62dd3cece77c7ff4
arojas00 <55304806+arojas00@users.noreply.github.com>
2020-07-22 11:10:08 -0600

    Resolves issue #3 Class Board

--- src/Battleship/Board.cpp
+++ src/Battleship/Board.cpp
@@ -1,6 +1,30 @@
+#include <iostream>
 #include "Board.h"
+#include "Cell.h"
 
-Board::Board()
-{
+Board::Board() {
+    for (int i = 0; i < dim; i++) {
+        for (int j = 0; i < dim; i++) {
+            tablero[i][j] = new Cell cell;
+        }
+    }
+}
+// Sets the all the cells pointer to null.
+void Board::clearGrid() {
+    for (int i = 0; i < dim; i++) {
+        for (int j = 0; i < dim; i++) {
+            tablero[i][j] = cell.clearPosition();
+        }
+    }
+}
+// Place the ship in the given position of the booard.
+void Board::setShips(int row, int col, int shipNumber) {
+    tablero[row][col] = cell.assignShip(shipNumber);
+}
+// Decrease the shipsLeft when the ships life gets to 0.
+int Board::foundShips() {
+    return shipsLeft -= 1;
+}
+void Board::updateBoard(bool a, int row, int col) {
 
 }
--- src/Battleship/Board.h
+++ src/Battleship/Board.h
@@ -1,11 +1,47 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "Cell.h"
+#include <iostream>
+#include <vector> 
 
+/**
+ *  Board class. Creates de board and place the ships of each player
+ */
 class Board
 {
+private:
+    int dim = 10;
+    int shipsLeft = 5;
+    Cell tablero[][];
+    std::vector<Ship> ship;
 public:
+    
     Board();
+    /*
+    @brief Sets the all the cells pointer to null.
+    @return Does not return anything.
+    */
+    void clearGrid();
+    /*
+    @brief Place the ship in the given position of the booard.
+    @param row Row choosen to place de ship.
+    @param col column choosen to place de ship.
+    @param shipNumber Identifies the choosen ship for placement.
+    @return Does not return anything.
+    */
+    void setShips(int row, int col, int shipNumber);
+    /*
+    @brief Decrease the shipsLeft when the ships life gets to 0.
+    @return The number of ships left.
+    */
+    int foundShips();
+    /*
+    @brief Shows the current state od the board.
+    @return Does not return anything.
+    */
+    void updateBoard(bool a, int row, int col);
+
 };
 
 #endif // BOARD_H
